c0bfcbc084d329b7475a2ea7cd487180c505e3df
Vishal Bhosale <72347843+webdev-Vishal@users.noreply.github.com>
2023-05-06 23:48:07 +0530

    buikd fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import  Header  from './header/header';
 import Hero from './hero/hero'
 import Footer from './footer'
