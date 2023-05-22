70728d0b7212b1a7651f2cdfe0083847808c0fee
Vishal Bhosale <72347843+webdev-Vishal@users.noreply.github.com>
2023-05-22 22:59:38 +0530

    header redesign

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,43 +1,27 @@
 import logo from '../../logo-transparent.png';
+import './header.scss';
 function Header() {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="/">
-				<img src={logo} class="img-fluid" width="100px" style- />
-			</a>
-			<a className="navbar-brand" href="/"></a>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-
-			<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-				<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li className="nav-item active">
-						<a className="nav-link" href="/">Home</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/">About us</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/">Events</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/">Donate us</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/">Be a Volunteer</a>
-					</li>
-				</ul>
-				<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li className="nav-item">
-						<a className="nav-link" href="/register-login">Login</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="/register-login">Signup</a>
-					</li>
-				</ul>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-2 col_logo">
+						<img src={logo}  alt=""/>
+					</div>
+					<div class="col-10 col_menu my-auto">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">About</a></li>
+							<li><a href="">Be A Volunteer</a></li>
+							<li><a href="">Events</a></li>
+							<li ><a href="">Career</a></li>
+							<li class="btn"><a href="">Login</a></li>
+							<li class="btn"><a href="">Signup</a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
-		</nav>
+		</header>
 	);
 }
 
