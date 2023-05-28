830e7392c5a93f2c9af9ba83dbab866f05562d62
Vishal Bhosale <72347843+webdev-Vishal@users.noreply.github.com>
2023-05-28 22:03:37 +0530

    etst

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home'; 
 import RegisterLogin from './pages/register-login/register-login';
-
+import Career from './pages/career/career';
 function App() {
   return (
     <>
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register-login" element={<RegisterLogin />} />
+        <Route path="/career" element={<Career />} />
       </Routes>
     </Router>
     </>
---
+++ src/pages/career/career.js
@@ -0,0 +1,14 @@
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import CareersForm from '../../components/careers-form/careers-form';
+function Career() {
+  return (
+    <>
+    <Header/>
+    <CareersForm/>
+    <Footer/>
+    </>
+  );
+}
+
+export default Career;
